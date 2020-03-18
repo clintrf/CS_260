ea2e842ee73b2bb2a883d76006847312fa40d87c
Clint Frandsen <clint.frandsen@gmail.com>
2020-03-18 02:45:11 -0600

    create2 done

--- create3.clintrfrandsen.com/create3lab/src/App.vue
+++ create3.clintrfrandsen.com/create3lab/src/App.vue
@@ -1,12 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+<div id="app">
+  <div id="menu">
+
+    <div id="side-left">
+      <router-link to="/browse">
+        <div class="menu-item browse">
+          <img src="../public/images/globe.png">
+          <p>Browse</p>
+        </div>
+      </router-link>
     </div>
-    <router-view/>
+
+    <div id="brand">
+      <router-link to="/">
+        <img src="../public/images/PokemonLogo.jpg">
+      </router-link>
+    </div>
+
+    <div id="side-right">
+      <router-link to="/deck">
+        <div class="menu-item">
+          <img src="../public/images/love.png">
+          <p>{{this.$root.$data.deck.length}} Deck</p>
+        </div>
+      </router-link>
+    </div>
+
   </div>
+  <router-view />
+</div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      numberOfItems: this.$root.$items.length,
+    }
+  },
+  computed: {
+    totalItems() {
+      return this.$root.$items.length;
+    }
+  },
+  methods: {
+    select(type1) {
+      this.type1 = type1;
+    },
+
+  }
+}
+</script>
+
 
 <style>
 #app {
@@ -29,4 +74,73 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 50px 100px;
+
+}
+
+#menu {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 5px;
+  grid-template-areas: "side-left brand side-right";
+  margin-bottom: 50px;
+}
+
+#menu a {
+  color: #B84901;
+}
+
+#brand {
+  grid-area: brand;
+  display: flex;
+  justify-content: center;
+}
+
+#brand img {
+  height: 200px;
+}
+
+#side-left {
+  grid-area: side-left;
+  display: flex;
+  justify-content: right;
+  margin-left: auto;
+margin-right: auto;
+}
+
+#side-left img {
+  width: 50px;
+}
+
+#side-right {
+  grid-area: side-right;
+  display: flex;
+  justify-content: right;
+  margin-left: auto;
+margin-right: auto;
+}
+
+#side-right img {
+  width: 50px;
+}
+
+.menu-item {
+  display: flex;
+  flex-direction: column;
+}
+
+.menu-item p {
+  margin: 0px;
+}
+
+.browse {
+  margin-right: 50px;
+}
+
 </style>
--- create3.clintrfrandsen.com/create3lab/src/main.js
+++ create3.clintrfrandsen.com/create3lab/src/main.js
@@ -2,9 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import pokemons from './pokemons.js'
+
 Vue.config.productionTip = false
+
+let data = {
+  pokemons: pokemons,
+  deck: [],
+  battle_deck: [],
+  winner: []
+}
 
 new Vue({
   router,
+  data,
   render: h => h(App)
 }).$mount('#app')
